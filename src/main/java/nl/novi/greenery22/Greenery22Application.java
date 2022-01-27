446b1b2a6e24fd35ad29ebdb9b8e9367ac10314f
ikbenwen <w.michels@novi-education.nl>
2022-01-27 16:54:28 +0100

    added dependencies

---
+++ src/main/java/nl/novi/greenery22/Greenery22Application.java
@@ -0,0 +1,13 @@
+package nl.novi.greenery22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Greenery22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Greenery22Application.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/greenery22/Greenery22ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.greenery22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Greenery22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
